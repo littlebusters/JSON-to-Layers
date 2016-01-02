e15e9c337c60b697f769f86d069434946f7ecfa5
littlebusters <little.busters.reply@gmail.com>
2016-01-03 01:50:58 +0900

    add contextSettings
    set opacity and blend mode, add CONST file

---
+++ JSON to Layers.sketchplugin/Contents/Sketch/lib/const.js
@@ -0,0 +1,19 @@
+var fw2json = fw2json || {};
+fw2json.blendMode = [];
+fw2json.blendMode['normal'] = 0;
+fw2json.blendMode['darken'] = 1;
+fw2json.blendMode['multiply'] = 2;
+fw2json.blendMode['colorBurn'] = 3;
+fw2json.blendMode['lighten'] = 4;
+fw2json.blendMode['screen'] = 5;
+fw2json.blendMode['colordodge'] = 6;
+fw2json.blendMode['overlay'] = 7;
+fw2json.blendMode['softlight1'] = 8;
+fw2json.blendMode['hardlight'] = 9;
+fw2json.blendMode['difference'] = 10;
+fw2json.blendMode['exclusion'] = 11;
+fw2json.blendMode['hue'] = 12;
+fw2json.blendMode['saturation'] = 13;
+fw2json.blendMode['color'] = 14;
+fw2json.blendMode['luminosity'] = 15;
+//average, dissolve, inversecolorburn, softburn, linearburn, inversecolordodge, softdodge, lineardodge, softlight2, vividlight, linearlight, pinlight, hardmix, negation, red, green, blue, reflect, glow, freeze, heat, additive, subtractive, subtract, interpolation, stamp, xor, invert, tint, erase
